577ec4db1b10d6c2a6783ed537ce08fa78a41198
je-pbx <johannes.erber@photobox.com>
2015-10-03 13:33:45 +0200

    Generate first files

---
+++ readme.md
@@ -0,0 +1,3 @@
+# My first elm app
+
+Application to follow along https://github.com/j-hannes/elm-tutorial.
---
+++ src/elm/app.elm
@@ -0,0 +1,7 @@
+import Html exposing (span, text)
+import Html.Attributes exposing (class)
+
+main =
+  span
+    [class "welcome-message"]
+    [text "Hello, World!"]
