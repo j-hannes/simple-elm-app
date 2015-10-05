cd527b0d746d6b5076e63fb4b1a63410fa121e6b
je-pbx <johannes.erber@photobox.com>
2015-10-05 13:24:48 +0200

    Add server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var del = require('del')
 var gulp = require('gulp')
 var elm = require('gulp-elm')
+var connect = require('gulp-connect')
 
 gulp.task('clean', function(callback) {
   del(['public'], callback)
@@ -8,8 +9,9 @@ gulp.task('clean', function(callback) {
 
 gulp.task('html', function() {
   gulp
-    .src('./src/html/index.html')
-    .pipe(gulp.dest('./public/'))
+    .src('src/html/index.html')
+    .pipe(gulp.dest('public'))
+    .pipe(connect.reload())
 })
 
 gulp.task('elm', function() {
@@ -17,4 +19,20 @@ gulp.task('elm', function() {
     .src('src/elm/app.elm')
     .pipe(elm())
     .pipe(gulp.dest('public'))
+    .pipe(connect.reload())
 })
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public',
+    livereload: true
+  })
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/elm/app.elm', ['elm'])
+  gulp.watch('src/html/index.html', ['html'])
+})
+
+gulp.task('build', ['clean', 'html', 'elm'])
+gulp.task('dev', ['build', 'connect', 'watch'])
